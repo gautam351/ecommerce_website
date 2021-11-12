bf72b18cda7a00a55ba9637fb918a5d58dbb4f95
gautam351 <gautampraveen351@gmail.com>
2021-11-12 17:35:37 +0530

    frontend 5hrs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './component/layout/Header/Header';
-import {BrowserRouter as Router} from "react-router-dom"
-import WebFont from 'webfontloader';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";import WebFont from 'webfontloader';
 import { useEffect } from 'react';
 import Footer from './component/layout/Header/Footer/Footer';
 
-
+import Home from "./component/Home/Home"
 
 function App() {
 
@@ -25,8 +29,14 @@ useEffect(() => {
   return (
    
 <>
-  <Router >
+
+  <Router > 
   <Header/>
+  <Routes>
+    <Route path="/" element={<Home/>} />
+
+  
+  </Routes>
   <Footer />
   </Router>
 </>
--- frontend/src/component/layout/Header/Footer/Footer.js
+++ frontend/src/component/layout/Header/Footer/Footer.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
-function Footer() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import "./Footer.css";
 
-export default Footer
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <div className="leftFooter">
+        <h4>DOWNLOAD OUR APP</h4>
+        <p>Download App for Android mobile phone</p>
+        <img
+          src="https://mern-stack-ecommerce-store.herokuapp.com/static/media/playstore.82b48319.png"
+          alt="playStore"
+        />
+      </div>
+
+      <div className="midFooter">
+        <h1>ECOMMERCE</h1>
+        <p>High Quality is our first priority</p>
+
+        <p>Copyrights &copy;{ new Date().getFullYear()}  </p>
+      </div>
+
+      <div className="rightFooter">
+        <h4>Follow Us</h4>
+       <InstagramIcon fontSize="large" />
+       <FacebookIcon fontSize="large"/>
+       <YouTubeIcon fontSize="large"/>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
